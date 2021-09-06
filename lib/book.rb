0093075ae3923fc2c7dc231d4302f28b0e87d878
Saloni <salonimehta2127@gmail.com>
2021-09-05 21:18:26 -0400

    Completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+
+    attr_reader:title
+
+    def initialize(title)
+        @title=title
+    end
+    attr_accessor:author
+    attr_accessor:page_count
+    attr_accessor:genre
+
+    def turn_page
+    puts "Flipping the page...wow, you read fast!"
+    end
+end
 
 
